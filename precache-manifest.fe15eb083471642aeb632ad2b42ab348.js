4f3184e69fd66745ba80c8dfbfda4616e551c2ad
CodyHoover <codydevloop@gmail.com>
2020-07-22 06:34:32 -0700

    Updates

---
+++ precache-manifest.fe15eb083471642aeb632ad2b42ab348.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c967f818a28ed13b7389b8dab438cad",
+    "url": "/React_Pokemon/index.html"
+  },
+  {
+    "revision": "342214dba639a6420517",
+    "url": "/React_Pokemon/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "21df5bf8b98ef73f7230",
+    "url": "/React_Pokemon/static/css/main.652a0a1a.chunk.css"
+  },
+  {
+    "revision": "342214dba639a6420517",
+    "url": "/React_Pokemon/static/js/2.2cc35893.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React_Pokemon/static/js/2.2cc35893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21df5bf8b98ef73f7230",
+    "url": "/React_Pokemon/static/js/main.37416729.chunk.js"
+  },
+  {
+    "revision": "f1064f333937d16ec013",
+    "url": "/React_Pokemon/static/js/runtime-main.74d6b578.js"
+  },
+  {
+    "revision": "cb525c5256ffce4d7e0e816c83219ef8",
+    "url": "/React_Pokemon/static/media/poke.cb525c52.JPG"
+  },
+  {
+    "revision": "f17a460a604614e2801802f80962e844",
+    "url": "/React_Pokemon/static/media/poke1.f17a460a.jpeg"
+  }
+]);
